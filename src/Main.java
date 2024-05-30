2cbf0db9dbfe9df69ac76fd288df0b85c103aad1
LeonelLexa <nartakhov@icloud.com>
2024-05-30 15:33:08 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BmiService service = new BmiService();
+        int weightKg = 90;
+        double heightM = 1.82;
+        int BMI = service.calculate(90, 1.82);
+        System.out.println(BMI);
     }
-    BmiService service = new BmiService();
-    int weightKg = 90;
-    double heightM = 1.82;
-    int BMI = (int) (int weightKg,double heightM);
-System.out.println(BMI);
-}
+
 }
